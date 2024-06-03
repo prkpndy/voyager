06849d40a22365185a1b37c36466cd029025a134
Prakhar Pandey <prkpndy@iitk.ac.in>
2024-06-03 23:41:55 +0530

    Update: Add util to fetch and save currency details and events

--- backend/transactions/utils.js
+++ backend/transactions/utils.js
@@ -9,6 +9,9 @@ async function fetchAndSaveTransaction(fastify, result, transactionHash) {
                 method: "starknet_getTransactionReceipt",
                 id: 1,
             }),
+            headers: {
+                "Content-Type": "application/json",
+            },
         });
 
         if (response.ok) {
@@ -27,6 +30,12 @@ async function fetchAndSaveTransaction(fastify, result, transactionHash) {
                 result.gas_consumed = result.actual_fee / result.l1_gas_price;
             }
 
+            if (transaction.events) {
+                result.num_events = transaction?.events?.length || 0;
+            } else {
+                result.num_events = 0;
+            }
+
             result.execution_resources = transaction.execution_resources;
             result.fetched_all = true;
 
@@ -36,16 +45,75 @@ async function fetchAndSaveTransaction(fastify, result, transactionHash) {
                 { upsert: true }
             );
         } else {
-            console.log(
+            fastify.log.error(
                 `Response for GET Transaction NOT OK for transactionHash: ${transactionHash}`
             );
         }
     } catch (error) {
-        console.log(
-            `Error fetching the GET Transaction for transactionHash = ${transactionHash}: `,
-            error
+        fastify.log.error(
+            `Error fetching the GET Transaction for transactionHash = ${transactionHash}`
         );
+        fastify.log.error(error);
     }
 }
 
-module.exports = { fetchAndSaveTransaction };
+async function fetchAndSaveConversionData(fastify, result) {
+    const conversionData = await fastify.db.voyager.Conversion.findOne({
+        id: 1,
+    });
+
+    if (
+        !conversionData ||
+        conversionData.wei_in_usd_fetch_time <
+            Date.now() - fastify.CONVERSION_FETCH_GAP
+    ) {
+        fastify.log.info(
+            "Conversion data is either stale or absent. Fetching new data from COINGECKO API..."
+        );
+
+        try {
+            const response = await fetch(fastify.COINGECKO_API_URL);
+
+            if (response.ok) {
+                const data = await response.json();
+                const usd_in_eth = data.ethereum.usd;
+
+                const wei_in_usd = 1e18 / usd_in_eth;
+                result.wei_in_usd = wei_in_usd;
+
+                if (!conversionData) {
+                    fastify.log.info(
+                        "Conversion data is absent. Creating new one..."
+                    );
+                    await fastify.db.voyager.Conversion.create({
+                        id: 1,
+                        wei_in_usd: wei_in_usd,
+                        wei_in_usd_fetch_time: Date.now(),
+                    });
+                } else {
+                    fastify.log.info(
+                        "Conversion data is stale. Updating it..."
+                    );
+                    await fastify.db.voyager.Conversion.updateOne(
+                        { id: 1 },
+                        {
+                            wei_in_usd: wei_in_usd,
+                            wei_in_usd_fetch_time: Date.now(),
+                        },
+                        { upsert: true }
+                    );
+                }
+            } else {
+                fastify.log.error(`Response for GET Conversion Data NOT OK`);
+            }
+        } catch (error) {
+            fastify.log.error(`Error fetching the GET Conversion Data`);
+            fastify.log.error(error);
+        }
+    } else {
+        fastify.log.info("Conversion data is available");
+        result.wei_in_usd = conversionData.wei_in_usd;
+    }
+}
+
+module.exports = { fetchAndSaveTransaction, fetchAndSaveConversionData };
