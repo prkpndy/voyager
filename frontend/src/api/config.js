c5a7e93f3feb5e284e170f4f469de2c8f3806bde
Prakhar Pandey <prkpndy@iitk.ac.in>
2024-06-04 11:51:55 +0530

    Update: Backend API URL

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,3 +1,4 @@
-const URL_PREFIX = "http://localhost:3000/api/v1";
+// const URL_PREFIX = "http://localhost:3000/api/v1";
+const URL_PREFIX = "https://voyager-78iy.onrender.com/api/v1";
 
 export default URL_PREFIX;
